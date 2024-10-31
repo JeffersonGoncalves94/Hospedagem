b387d116b46a2d5405392bf3d92047aaeac437c0
https://github.com/JeffersonGoncalves94.git <https://github.com/JeffersonGoncalves94/testes.git>
2024-10-31 13:22:33 -0300

    hotel

---
+++ MauiAppHospedagem/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿
+namespace MauiAppHospedagem;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new NavigationPage(new views.hospedagemContratada());
+	}
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+		var window = base.CreateWindow(activationState);
+		window.Width = 480;
+		window.Height = 640;
+
+
+        return window;
+  }
+}
---
+++ MauiAppHospedagem/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiAppHospedagem;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
